5a1c9200d0ae9a9f7d87fd60d1d2321409eee523
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-03 05:30:07 +0200

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,11 +13,3 @@ def index_range(page, page_size) -> tuple:
     start_index = (page-1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
-
-res = index_range(1, 7)
-print(type(res))
-print(res)
-
-res = index_range(page=3, page_size=15)
-print(type(res))
-print(res)
