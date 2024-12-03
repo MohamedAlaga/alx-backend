345bd0eb030341ff8469b2207c2503d5780bc46d
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-03 05:29:29 +0200

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,19 @@ module contains index_range function
 
 
 def index_range(page, page_size) -> tuple:
-    start_index = page * page_size
+    """
+    :param page: page number
+    :param page_size: the size of the page
+    :return: tuple containing start and end index
+    """
+    start_index = (page-1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
+
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
