987329cd390d0a7fd13ebc553a527f95b9d56d8b
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-09 10:58:29 +0200

    6. Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -29,31 +29,15 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
-    """
-    Validate user login details
-    Args:
-        id (str): user id
-    Returns:
-        (Dict): user dictionary if id is valid else None
-    """
-    return users.get(int(id), {})
-
-
 @babel.localeselector
 def get_locale() -> str:
     """
     Gets locale from request object
     """
-    options = [
-        request.args.get('locale', '').strip(),
-        g.user.get('locale', None) if g.user else None,
-        request.accept_languages.best_match(app.config['LANGUAGES']),
-        Config.BABEL_DEFAULT_LOCALE
-    ]
-    for locale in options:
-        if locale and locale in Config.LANGUAGES:
-            return locale
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 users = {
@@ -62,6 +46,17 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Validate user login details
+    Args:
+        id (str): user id
+    Returns:
+        (Dict): user dictionary if id is valid else None
+    """
+    return users.get(int(id), 0)
 
 
 @app.before_request
@@ -77,7 +72,7 @@ def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
