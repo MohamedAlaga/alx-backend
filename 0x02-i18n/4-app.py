1e57d709c832fca4b7993fe4a47c16f17d64ac89
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-09 10:48:47 +0200

    4. Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,7 +25,7 @@ def get_locale():
     """
     select the best locale from request
     """
-    locale = request.args.get("locale",'').strip()
+    locale = request.args.get("locale", '').strip()
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(["fr", "en"])
